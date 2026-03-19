50c24c7773c6b3e8ceb08e94e34254787a1227d0
devjeswani07-create <devjeswani07@gmail.com>
2026-03-19 18:11:46 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,15 @@
-const C='cx-v1',A=['/index.html','/manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+var CACHE = "captora-v1";
+var FILES = ["./index.html", "./manifest.json"];
+self.addEventListener("install", function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES)}));
+  self.skipWaiting();
+});
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}));
+  }));
+  self.clients.claim();
+});
+self.addEventListener("fetch", function(e){
+  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}));
+});
